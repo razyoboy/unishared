a8cdf477d397a26fcfa3d3e07f0ba9602b7979f3
razyoboy <razoreiei@gmail.com>
2021-03-22 20:00:27 +0700

    added something

---
+++ diff.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def func(x):
+    return np.exp(-x/4)*(2-x)-1
+
+def secant(func, XL, XR, tol):
+    fL = func(XL)
+    fR = func(XR)
+    if fL*fR >= 0:
+        print("nno")
+
+    root = (((XL*fR) - (XR*fL)) / (fR - fL))
+    funcroot = func(root)
+
+    while abs(funcroot) > tol:
+        if fL * funcroot >= 0:
+            XL = root
+            fL = funcroot
+        else: XR = root
+
+        root = (((XL*fR) - (XR*fL)) / (fR - fL))
+        funcroot = func(root)
+    else: return root
+
+res = secant(func, -2,2,0.00000001); print(res)
