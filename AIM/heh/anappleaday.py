d65b6eefc3b7f7f84ecbcc8e59c695d0e372039e
razyoboy <razoreiei@gmail.com>
2021-04-24 20:36:24 +0700

    I know I hated commenting but fuck it this time

--- AIM/heh/anappleaday.py
+++ AIM/heh/anappleaday.py
@@ -6,20 +6,28 @@ import matplotlib.pyplot as plt
 folder = 'F:/whateverthisis/Basic-Image-Processing/images/'
 question = folder + 'apples02.jpg'
 
+original = cv2.imread(question)
+#Converting BGR to RGB for comparision
+original = cv2.cvtColor(original, cv2.COLOR_BGR2RGB)
 image = cv2.imread(question)
+#Convert the color mode to HSV for ease of manipulation
 image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
+#So that we can compare both images
 out = image.copy()
-height, width, depth = out.shape
+#Spliting the three channels and increasing the hue channel for the green color
 h, s, v = cv2.split(out)
 h += 55
 
+#Combine them back into a 3-channel image
 greenlantern = cv2.merge([h,s,v])
-out = cv2.cvtColor(greenlantern, cv2.COLOR_HSV2BGR)
+#Converting HSV back into RGB color space
+out = cv2.cvtColor(greenlantern, cv2.COLOR_HSV2RGB)
 
 f, axs = plt.subplots(figsize=(15,15))
 plt.subplot(121)
-plt.imshow(image)
+plt.imshow(original); plt.title('Original')
 plt.subplot(122)
-plt.imshow(out)
+plt.imshow(out); plt.title('Manipulated')
+plt.savefig("ApplesGreen"+".png", format="PNG", bbox_inches='tight')
 plt.show()
--- AIM/heh/mitosis.py
+++ AIM/heh/mitosis.py
@@ -8,36 +8,64 @@ files = os.listdir(folder)
 
 total = len(files)
 f, axs = plt.subplots(figsize=(15,total*6))
+n = 0 #For saving and naming files
+imgdb1 = [] #DB for filenames of each images
+imgdb2 = [] #DB for processed images
+imgdb3 = [] #DB for contoured images
+imgdb4 = [] #DB for number of contours (cells)
 
 for i, file in enumerate(files):
     image = cv2.imread(folder + file)
+    #Converting BGR to HSV for ease of manipulation
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    #Increasing the intensity of the images
     hsv = image * 15
-    gray = hsv.copy()
-    opening = hsv.copy()
+    gray = hsv.copy() #Preparation to be contoured
+    opening = hsv.copy() #Preparation to be contoured as well
 
+    #Converting the now seeable images into Grayscale
     grayit = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
+    #Data for creating matrix of ones
     height, width = grayit.shape
 
+    #Creating threshold level for cutting and rounding color levels
     ret, thresh = cv2.threshold(grayit, 80, 255, cv2.THRESH_BINARY)
 
+    #To create a color inverted image for ease of reading for the computer
     inverted = cv2.bitwise_not(thresh)
     kernel = np.ones((50,50), np.uint8)
     opening = cv2.morphologyEx(inverted, cv2.MORPH_OPEN, kernel)
-
+    
+    #Creating contours
     count, hiera = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     bord = np.zeros([height,width,3])
 
     cv2.drawContours(bord, count, -1,(0,255,0),3)
 
-    plt.subplot(total, 2, 2 * i + 1)
-    plt.title(file)
-    plt.imshow(gray, cmap=plt.cm.gray)
-    plt.subplot(total, 2, 2 * i + 2)
-    plt.title('count = ' + str(len(count)))
-    plt.imshow(opening, cmap=plt.cm.gray)
+    #For appending each corresponding data into an array
+    imgdb1.append(file)
+    imgdb2.append(gray)
+    imgdb3.append(opening)
+    imgdb4.append(len(count))
 
-plt.show()
+
+#   The above data and results are stored in an array so that images 
+#   can be easily saved and exported without taking to much time and effort
+
+for j in range(len(imgdb1)):
+    plt.subplot(121)
+    plt.title(imgdb1[j]) #Calling the j element for filenames
+    plt.imshow(imgdb2[j], cmap=plt.cm.gray) #Calling the j element for processed images
+    plt.subplot(122)
+    
+    #Calling the j element for the number of contours found
+    plt.title('No. of cells = ' + str(imgdb4[j]))
+    plt.imshow(imgdb3[j], cmap=plt.cm.gray)
+    #n is simply there for saving files with different filenames
+    n += 1
+    plt.savefig("Cont"+str(n)+".png", format="PNG", bbox_inches='tight')
+
+
 
 
 
