be7e87807b1248d750a569c5c2904182de8a788f
Kjmtz <thanachotkim@hotmail.co.th>
2021-05-15 14:38:28 +0700

    'f'

---
+++ FINAL/horny/cremer.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+a = int(input("size of matrix >> "))
+mat = np.ones([int(a), int(a)])
+newmat = np.ones([int(a), int(a)])
+res = np.ones([int(a),1])
+
+
+
+for i in range(0,a,1):
+    for j in range(0,a,1):
+        print(f"Position {i+1} * {j+1} ") #that must be  of the equation
+        v = int(input("the coefficient >> "))
+        mat[i,j] = v
+        newmat[i,j] = v
+
+#print(np.linalg.det(mat))
+
+for i in range(0,a,1):
+    print(f"Position {i+1} * {1}")
+    u = int(input("the result >> "))
+    res[i,0] = u # careful the size is not what you thought
+
+#b = int(input("the indent of x we want to calculate >> ")) #begin with x0,x1, .... , 
+                                                            #xn >>>>> careful...
+                                                            # if you want to find x0 put 0 into the input
+
+for i in range(0,a,1):
+    for j in range(0,a,1):
+        newmat[j, i] = res[j,0]
+        if j+1 == a : 
+            #print("first> \n", newmat)  
+            #print("mat >\n", mat) 
+            ans = np.linalg.det(newmat)/np.linalg.det(mat)
+            print(f"The value of x{i} is {ans}")
+            newmat[:,:] = mat[:,:]
+        
+
+
+
+
+
+      
+
+
--- FINAL/horny/testonly.py
+++ FINAL/horny/testonly.py
@@ -1,5 +1,6 @@
-from this import d
+
 import numpy as np
+"""
 a = [0,0,0,0,0]
 a.insert(2,1)
 print(a)
@@ -10,3 +11,10 @@ c = input(">>")
 d = int(c)
 h =d
 print(h)
+"""
+mat = np.ones([3,3])
+ne = np.ones([3,3])
+ne[2,0] = 2
+print(ne)
+ne = mat
+print(ne)
